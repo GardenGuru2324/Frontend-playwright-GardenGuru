89f2540f395d3746b914d24cfc670ea1304a2bb8
MilanMareels <125263673+MilanMareels@users.noreply.github.com>
2024-06-30 10:19:35 +0200

    Tests for GURU-24 (#1)
    * login tests done
    
    * Register tests done

--- fixtures/pages.fixture.ts
+++ fixtures/pages.fixture.ts
@@ -1,12 +1,22 @@
-import { test as base } from '@playwright/test';
-import { HomePage } from '../pages/homePage';
+import { test as base } from "@playwright/test";
+import { HomePage } from "../pages/homePage";
+import { LoginPage } from "../pages/loginPage";
+import { RegisterPage } from "../pages/registerPage";
 
 type PageFixture = {
-	homePage: HomePage;
+  homePage: HomePage;
+  loginPage: LoginPage;
+  registerPage: RegisterPage;
 };
 
 export const test = base.extend<PageFixture>({
-	homePage: async ({ page }, use) => {
-		await use(new HomePage(page));
-	}
+  homePage: async ({ page }, use) => {
+    await use(new HomePage(page));
+  },
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  registerPage: async ({ page }, use) => {
+    await use(new RegisterPage(page));
+  },
 });
