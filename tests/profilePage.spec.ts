cbef441bafe9638e0cc84910cfcb9b531f51ae4d
Renzo-Declerck <declerck.renzo@outlook.com>
2024-09-03 15:28:42 +0200

    fix: adding navigation to the profile page in every test of the profile page

--- tests/profilePage.spec.ts
+++ tests/profilePage.spec.ts
@@ -9,8 +9,8 @@ test.describe("Profile Page", () => {
   let navigationAction: NavigationAction;
 
   test.beforeEach(async ({ page }) => {
-    const navigationAction = new NavigationAction(page);
-    const loginAction = new LoginPageAction(page);
+    navigationAction = new NavigationAction(page);
+    loginAction = new LoginPageAction(page);
 
     await page.goto(pageRoutes.loginPage);
     await loginAction.loginPlayWrightTestuser();
@@ -25,24 +25,32 @@ test.describe("Profile Page", () => {
   });
 
   test("Profile page should have correct top section", async ({ profilePage }) => {
+    await navigationAction.navigateToPage("profile");
+    
     const topSection = await profilePage.topSection;
 
     await expect(topSection).toBeVisible();
   });
 
   test("Profile page should have correct info section", async ({ profilePage }) => {
+    await navigationAction.navigateToPage("profile");
+
     const infoSection = await profilePage.infoSection;
 
     await expect(infoSection).toBeVisible();
   });
 
   test("User should see own profile picture", async ({ profilePage }) => {
+    await navigationAction.navigateToPage("profile");
+    
     const profilePicture = await profilePage.profilePicture;
 
     await expect(profilePicture).toBeVisible();
   });
 
   test("User can change profile picture", async ({ profilePage }) => {
+    await navigationAction.navigateToPage("profile");
+    
     const editProfilePictureButton = await profilePage.editProfilePictureButton;
 
     // Nog geen functionele werking van deze knop, dus voorlopig enkel testen op zichtbaarheid
@@ -50,9 +58,11 @@ test.describe("Profile Page", () => {
   });
 
   test("User can logout", async ({ profilePage }) => {
+    await navigationAction.navigateToPage("profile");
+    
     const logoutButton = await profilePage.logoutButton;
+    
     await logoutButton.click();
-
     await profilePage.page.waitForTimeout(1000);
 
     const isAuthenticated = await profilePage.page.evaluate(() => localStorage.getItem("isAuthenticated") === null);
